415bf6b5f7c2ba8c788dda22fd2c3e29fcf6f96d
yodofaither <yodofaither@outlook.com>
2021-04-11 11:47:57 +0800

    add react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+//import logo from './logo.svg';
+//import { Counter } from './features/counter/Counter';
+//import { Router,Route } from 'react-router';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './Home';
+import Homeexact from './Homeexact';
+import Page1 from './Page1.js';
+import Page2 from './Page2.js';
 import { DatePicker } from 'antd';
 import axios from 'axios'
-import createSagaMiddleware from 'redux-saga'
+//import createSagaMiddleware from 'redux-saga'
 import './App.css';
 import 'antd/dist/antd.css';
 
@@ -16,54 +22,69 @@ function App() {
     console.log(error);
   });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          <DatePicker />
-        </span>
-      </header>
-    </div>
+    <Router >
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <Counter />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p> */}
+          <span>
+            <Route path="/" component={Home} />
+            <Route exact path="/" component={Homeexact} />
+            <Route path="/Page1" component={Page1} />
+            <Route path="/Page2" component={Page2} />
+            <DatePicker />
+            <a
+              className="App-link"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              子页面2
+            </a>
+            {/* <span>Learn </span>
+            <a
+              className="App-link"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux-toolkit.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux Toolkit
+            </a>
+            ,<span> and </span>
+            <a
+              className="App-link"
+              href="https://react-redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React Redux
+            </a> */}
+            
+          </span>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ my-app/src/Homeexact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+class Homeexact extends React.Component{
+render(){
+return(
+<div>
+<div>This is Home!</div>
+</div>
+);
+}
+}
+ 
+export default Homeexact;
