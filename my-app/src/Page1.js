21200e2d3bec24071fa77997389830a48170ca08
yodofaither <yodofaither@outlook.com>
2021-04-11 12:26:37 +0800

    successfully add less

--- my-app/src/Page1.js
+++ my-app/src/Page1.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import styles from './Page1.module.less'
  
 class Page1 extends React.Component{
 render(){
+    console.log(styles.page1class)
 return(
 <div>
-<div>This is Page1!</div>
+<div className={styles.page1class}>This is Page1!</div>
 </div>
 );
 }
