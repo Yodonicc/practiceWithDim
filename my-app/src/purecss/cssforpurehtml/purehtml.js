daa25e043da527f752dfc94e12d8c14b9e4ffb51
yodofaither <yodofaither@outlook.com>
2021-04-13 16:18:14 +0800

    学习基础DOM API parentNode.childNodes[10].style

--- my-app/src/purecss/cssforpurehtml/purehtml.js
+++ my-app/src/purecss/cssforpurehtml/purehtml.js
@@ -4,6 +4,8 @@ import styles from './purehtml.module.less'
 class Purehtml extends React.Component{
     bikeHandler(){
         //alert("bikeHandler");
+        document.getElementById('bikecheckbox').parentNode.parentNode.childNodes[10].style.display="block";
+        // document.getElementById('biketext2').style.display="block"
     }
     render(){
         return(
@@ -12,8 +14,8 @@ class Purehtml extends React.Component{
                 <div className={styles.middle}>This is Purehtml center!</div>
                 <div className={styles.right}>This is Purehtml right!</div>
                 <div>
-                    <input onClick={this.bikeHandler} className={styles.checkbox} type="checkbox" name="vehicle" value="Bike"></input>
-                    <span className={styles.myspan} onClick={this.bikeHandler}>我是bike复选框，点我！</span>
+                    <input id="bikecheckbox" onClick={this.bikeHandler} className={styles.checkbox} type="checkbox" name="vehicle" value="Bike"></input>
+                    <span className={styles.myspan}>我是bike复选框，点我！</span>
                     <div className={styles.code}>我是bike控制的文本！</div>
                 </div>
                 <div onClick={this.bikeHandler}>
@@ -29,7 +31,7 @@ class Purehtml extends React.Component{
                 <input type="file"></input>
                 {/* <input type="submit" className={styles.checkbox}></input> */}
                 <select></select>
-                <div className={styles.code}>我是bike控制的文本！</div>
+                <div className={styles.code} id="biketext2">我是bike控制的文本！</div>
                 <div>我是car控制的文本！</div>
                 <div>我是bus控制的文本！</div>
             </div>
